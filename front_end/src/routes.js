35da47b887a4b515183f2a491e5d6e38575c129f
DuongNguyen3c13 <duongnx@localhost.localdomain>
2020-10-01 00:20:07 +0700

    added frontend route

--- front_end/src/routes.js
+++ front_end/src/routes.js
@@ -3,8 +3,17 @@ import LibraryMusicIcon from '@material-ui/icons/LibraryMusic';
 
 // core components/views for Admin layout
 import Video from "./views/Video/index";
+import VideoCreate from "./views/Video/Create";
 
 const dashboardRoutes = [
+    {
+        path: "/videos/create",
+        name: "Videos",
+        icon: '',
+        isSub: true,
+        component: VideoCreate,
+        layout: ""
+    },
     {
         path: "/videos",
         name: "Videos",
